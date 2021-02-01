d2126020846f2f54b0140ebd8b2a9a6d96e63e29
Alexanderwebsot <64553081+Alexanderwebsot@users.noreply.github.com>
2021-02-01 17:54:17 +0300

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".phone").mask("+7 (999) 999-9999");
+  $('.header-slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true
+  });
+});
