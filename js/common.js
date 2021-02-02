4b02c9838b099b1f95b410eb0a1806a93922aace
Alexanderwebsot <64553081+Alexanderwebsot@users.noreply.github.com>
2021-02-02 05:46:08 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -6,4 +6,28 @@ $(document).ready(function () {
     slidesToScroll: 1,
     dots: true
   });
+  $('.catalog-slider-content').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    variableWidth: true,
+    arrows: false
+  });
+  $('.brends-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    variableWidth: true,
+    arrows: false
+  });
+  $('.popular-slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    arrows: true,
+    dots: true,
+    prevArrow: $('.popular-arrow__left'),
+    nextArrow: $('.popular-arrow__right')
+  });
 });
